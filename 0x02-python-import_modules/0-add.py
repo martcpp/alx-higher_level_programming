bd380b3f93aaab421341631f67181ca74d81f829
martcpp <mordecaiemmanueletukud@gmail.com>
2022-08-25 14:44:00 -0700

    done

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 from add_0 import add
 
@@ -11,17 +10,3 @@ if __name__ == "__main__":
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-=======
-#!/usr/bin/python3
-from add_0 import add
-
-if __name__ == "__main__":
-    """
-
-    Prints the result of the addition between two numbers
-
-    """
-    a = 1
-    b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
->>>>>>> e6a1519cf2a5ea313e638e2ec2d0192cf28c483d
